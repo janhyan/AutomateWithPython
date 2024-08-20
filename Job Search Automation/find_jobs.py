c5a8ea859a3d8b544e705d284def3669f095b368
Hyan <hyanxjan@gmail.com>
2024-08-20 10:26:12 -0400

    Started VM start automation

--- Job Search Automation/find_jobs.py
+++ Job Search Automation/find_jobs.py
@@ -5,27 +5,29 @@ import re
 
 # Get keyword to search from the user
 # keyword = input("Enter job keyword to search:")
-keyword = 'aws'
+keyword = "aws"
+headers = {
+    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+}
+
 
 class Indeed:
     def __init__(self, keyword):
-        self.url = f'https://ph.indeed.com/jobs?q={keyword}&l=Philippines&sc=0kf%3Ajt(new_grad)%3B&sort=date&start='
-        
-            
+        self.url = f"https://www.glassdoor.com/Job/philippines-aws-jobs-SRCH_IL.0,11_IN204_KO12,15.htm?seniorityType=entrylevel&remoteWorkType=1"
+
     def openUrl(self):
         webbrowser.open(self.url)
 
     def jobTotal(self, soup):
-        spans = soup.find_all("span", attrs={'class': None})
+        spans = soup.find_all("span", attrs={"class": None})
         print(spans)
-        
+
         for list in enumerate(spans):
             print(list)
             # print(re.findall('\d', list))
-            
 
     def scrapePage(self):
-        scrapePage = requests.get(self.url)
+        scrapePage = requests.get(self.url, headers)
         soup = BeautifulSoup(scrapePage.text, "lxml")
         self.jobTotal(soup)
 
---
+++ VM Automation/openVM.py
@@ -0,0 +1,3 @@
+import subprocess
+subprocess.call('D:\Program Files\Oracle\VirtualBox')
+
