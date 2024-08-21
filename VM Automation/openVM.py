6e30a86a18bbe73e83a777918c322a6e99b56822
Hyan <hyanxjan@gmail.com>
2024-08-21 10:58:13 -0400

    Completed automated VM open

--- VM Automation/openVM.py
+++ VM Automation/openVM.py
@@ -1,3 +1,10 @@
 import subprocess
+import pyautogui
+
 subprocess.call('D:\Program Files\Oracle\VirtualBox')
 
+def clickVM():
+    pyautogui.moveTo(1498, 70)
+
+    if pyautogui.pixel(1498, 70)[0] > 150:
+        pyautogui.click()
